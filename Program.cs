eec02eb78c9adaf1bca59cc0c5bf0f9d21ee5da2
Mikhail <Red-fox-91@mail.ru>
2023-12-01 21:12:36 +0300

    load

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿ int number = new Random().Next(100, 1000); // 100-999
+
+ int firstDigit = number % 100; 
+ int thirdDigit = number / 10; 
+ int result = firstDigit / 10; 
+ Console.WriteLine($"{number} -> {result}");
